433b1f408fdf98e52b7b58bb3f1e26ab3178b4fc
Rimon Koroni <rimon.k@clic.agency>
2019-01-08 15:02:44 +0200

    Initial Commit

---
+++ SwiftForm/SwiftForm.h
@@ -0,0 +1,19 @@
+//
+//  SwiftForm.h
+//  SwiftForm
+//
+//  Created by Rimon koroni on 1/8/19.
+//  Copyright © 2019 Rimon Koroni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftForm.
+FOUNDATION_EXPORT double SwiftFormVersionNumber;
+
+//! Project version string for SwiftForm.
+FOUNDATION_EXPORT const unsigned char SwiftFormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftForm/PublicHeader.h>
+
+
